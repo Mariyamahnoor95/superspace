e06d92f8782180e7cfa661e4705a52cd47f1f715
Umair Khan <umairkhan.cis@gmail.com>
2020-04-18 20:16:23 +0400

    Fix capitalizations

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,25 +1,25 @@
 # this is the guess the number game
 import random
-print('hello . what is your name?')
+print('Hello! What is your name?')
 name = input()
 secretNumber = random.randint(1,20)
-print('well,' + name + ',i am thinking of the number between 1-20')
+print('Well,' + name + ',I am thinking of the number between 1-20')
 
 # ask the player to guess 6 times,
 for guessesTaken in range (1,7):
-    print('take a guess')
+    print('Take a guess')
     guess = int(input())
     if guess < secretNumber:
-        print('your guess is too low')
+        print('Your guess is too low')
     elif guess > secretNumber:
-        print('your is too high')
+        print('Your is too high')
     else:
         break
 
 if guess == secretNumber:
-    print('good job...!!!' + name + ' you guessed my number in  ' + str(guessesTaken) + ' guesses')
+    print('Good Job...!!!' + name + ' you guessed my number in  ' + str(guessesTaken) + ' guesses')
     print('Thanks')
 
 else:
-    print('nope the number i was thinking was ' + str(secretNumber))
-    print('try again')
+    print('Nope the number i was thinking was ' + str(secretNumber))
+    print('Try Again!')
